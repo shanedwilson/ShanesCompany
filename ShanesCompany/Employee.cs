d8c59b37bde11f29e0820ba41e073bd1f6f60d3f
Shane D Wilson <me@shanedwilson.com>
2019-03-06 20:58:27 -0600

    working

---
+++ ShanesCompany/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShanesCompany
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime HiredOn { get; set; }
+
+        public void AddFirstName(string firstName)
+        {
+            FirstName = firstName;
+        }
+
+        public void AddLastName(string lastName)
+        {
+            LastName = lastName;
+        }
+
+        public void AddTitle(string title)
+        {
+            Title = title;
+        }
+
+        public void AddHireDate(DateTime hiredOn)
+        {
+            HiredOn = hiredOn;
+        }
+
+        public Employee(string firstName, string lastName, string title, DateTime hiredOn)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Title = title;
+            HiredOn = hiredOn;
+        }
+    }
+}
--- ShanesCompany/Program.cs
+++ ShanesCompany/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ShanesCompany
 {
-    class Program
+    public class Company
     {
+        /*
+            Some readonly properties
+        */
+        public string CompanyName { get; }
+        public DateTime CreatedOn { get; }
+
+        // Create a property for holding a list of current employees
+        public List<Employee> Employees { get; set; }
+
+        /*
+        Create a constructor method that accepts two arguments:
+        1. The name of the company
+        2. The date it was created
+
+         The constructor will set the value of the public properties
+        */
+
+        public Company (string companyName, DateTime createdOn)
+        {
+            CompanyName = companyName;
+            CreatedOn = createdOn;
+            Employees = new List<Employee>();
+        }
+
+        // Create a method that allows external code to add an employee
+        public void addEmployee(Employee newEmployee)
+        {
+            Employees.Add(newEmployee);
+        }
+
+        // Create a method that allows external code to remove an employee
+        public void removeEmployee(Employee exEmployee)
+        {
+            Employees.Remove(exEmployee);
+        }
+
+        public void listEmployees(List<Employee> employees)
+        {
+            foreach(Employee employee in employees)
+                Console.WriteLine(employee.FirstName);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Company bigTech = new Company("Big Tech", DateTime.Now);
+
+            Employee shane = new Employee("Shane D", "Wilson", "Junior Dev", DateTime.Now);
+            Employee nazz = new Employee("Nazz", "Wilson", "Teenager", DateTime.Now);
+            Employee stacye = new Employee("Stacye", "Wilson", "President", DateTime.Now);
+
+            bigTech.addEmployee(shane);
+            bigTech.addEmployee(stacye);
+            bigTech.addEmployee(nazz);
+
+            bigTech.listEmployees(bigTech.Employees);
+
+            Console.ReadKey();
         }
     }
 }
